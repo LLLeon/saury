423e4e975cb5cb727c0fe25b1c6e6b352099efda
LLLeon <jiachenhui1989@gmail.com>
2018-08-13 12:11:29 +0800

    container/list: add a simple singly list

---
+++ container/list/singly/list.go
@@ -0,0 +1,52 @@
+package singly
+
+// ListNode represents a node of a singly linked list.
+type ListNode struct {
+	Value int
+	Next  *ListNode
+}
+
+// NewList creates a new node.
+func NewList() *ListNode {
+	return new(ListNode)
+}
+
+// NextNode returns the node after the ln.
+func (ln *ListNode) NextNode() *ListNode {
+	return ln.Next
+}
+
+// PushFront pushes val to the top of the list and return val.
+func (ln *ListNode) PushFront(val int) int {
+	ln.Value = val
+	return val
+}
+
+// InsertAfter inserts the node with val after ln and return val.
+func (ln *ListNode) InsertAfter(val int) int {
+	return insert(val, ln)
+}
+
+// InsertAt inserts the node with val after at and return the node.
+func InsertAt(val int, at *ListNode) *ListNode {
+	insert(val, at)
+	return at.Next
+}
+
+func insert(val int, at *ListNode) int {
+	if at.Next == nil {
+		next := new(ListNode)
+		next.Value = val
+		at.Next = next
+	} else {
+		oldNext := at.Next
+		newNext := new(ListNode)
+
+		newNext.Value = val
+		newNext.Next = oldNext
+
+		at.Next = newNext
+	}
+
+	return val
+}
